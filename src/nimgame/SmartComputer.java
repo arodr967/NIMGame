51f9271322ff12ed000e018bee847ef2e0f1bc28
Alicia Rodriguez <arodr967@fiu.edu>
2015-07-03 12:37:07 -0400

    Cheater proofed

--- src/nimgame/SmartComputer.java
+++ src/nimgame/SmartComputer.java
@@ -1,22 +1,22 @@
 /*
 File Name: SmartComputer.java
-
+//******PROPERTY OF ALICIA RODRIGUEZ********
 Smart computer player removes exactly enough marbles to make the remaining pile
 a power of 2 minus one (ie. 1, 3, 7, 15, 31, or 63).
 The smart computer player cannot be beaten unless the initial pile number that
 is generated happens to be a power of 2 minus one.
  */
 package nimgame;
-
+//******PROPERTY OF ALICIA RODRIGUEZ********
 import java.util.Random;
-
+//******PROPERTY OF ALICIA RODRIGUEZ********
 public class SmartComputer implements Player
 {
     /**
      * Constructor of the smart computer
      */
     public SmartComputer()
-    {
+    {//******PROPERTY OF ALICIA RODRIGUEZ********
         //no instances variables needed; Therefore, nothing to be initialized.
     }
     
@@ -29,7 +29,7 @@ public class SmartComputer implements Player
     public String toString() 
     {
         return "Smart Computer";
-    }
+    }//******PROPERTY OF ALICIA RODRIGUEZ********
     
     /**
      * The smart computer will remove exactly enough marbles to make the 
@@ -40,7 +40,7 @@ public class SmartComputer implements Player
      */
     @Override
     public int move(int marbles)
-    {
+    {//******PROPERTY OF ALICIA RODRIGUEZ********
         int move=0;
         
         if(marbles == 1 || marbles == 3 || marbles == 7 || marbles == 15
@@ -50,14 +50,14 @@ public class SmartComputer implements Player
             //then play as a dumb computer
             Random generator = new Random();
             move = generator.nextInt(marbles/2)+1;
-            
+            //******PROPERTY OF ALICIA RODRIGUEZ********
         }
         else //play as smart computer
         {
             //powers of 2 -1
             int [] powers = {1, 3, 7, 15, 31, 63};
                            //0  1  2   3   4   5
-        
+        //******PROPERTY OF ALICIA RODRIGUEZ********
             int i = powers.length-1;        //last position in array
         
             //while in position i
@@ -65,7 +65,7 @@ public class SmartComputer implements Player
             {   //if i is > marbles
                 //decrement i until you've found that i is greater than marbles
             i--;
-            
+            //******PROPERTY OF ALICIA RODRIGUEZ********
             //Example: marbles = 100
             //is 63 greater than 100? no
             
@@ -73,11 +73,11 @@ public class SmartComputer implements Player
             //then subtract 100 by 63, which will give you the number of moves
         
             move = marbles - powers[i];
-        
+        //******PROPERTY OF ALICIA RODRIGUEZ********
         }
         
         System.out.println(this + " has removed " + move 
                 + " marbles from the pile.\n");
         return move;    //move = marbles - powers(which ever number selected)
-    }
-}
+    }//******PROPERTY OF ALICIA RODRIGUEZ********
+}//end of SmartComputer class definition
